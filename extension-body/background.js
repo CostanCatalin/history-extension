25cb2a2f9e213e0dca1f9224a221803a038af1c7
Catalin Costan <catalin.costan1@gmail.com>
2018-01-14 20:21:36 +0200

    update on dom for each visit

--- extension-body/background.js
+++ extension-body/background.js
@@ -11,21 +11,64 @@ function initApp() {
 
   firebase.auth().onAuthStateChanged(function(user) {
     if (user) {
+      var currentUrl;
+
       userId = firebase.auth().currentUser.uid;
+      var usersRef = firebase.database().ref('/users/' + userId);
 
       // If new user, add default colour
-      firebase.database().ref('/users/' + userId).once('value', function(snapshot) {
-        if (snapshot.val() ==  null) {
-          firebase.database().ref('users/' + userId).set({
+      usersRef.once('value', function(snapshot) {
+        if (snapshot.val() == null) {
+          usersRef.child(userId).set({
             highlight_color: "#fff385"
           });
         }
       });
-    }    
-    else{
+
+      // check if url in "followed" links
+      chrome.tabs.query({
+          'active': true,
+          'windowId': chrome.windows.WINDOW_ID_CURRENT
+        },
+        function(tabs) {
+          currentUrl = tabs[0].url;
+
+          // Url doesn't exist
+          usersRef.child("/history/").orderByChild('url')
+            .equalTo(currentUrl).once('value', function(snapshot) {
+
+            // Add dom for this URL if not existing
+            if (!snapshot.exists()) {
+
+              var newKey = usersRef.child("/history/").push().key;
+
+              usersRef.child("/history/" + newKey).set({
+                url: currentUrl,
+                dom: "test"
+              });
+
+              // Update URL dom json
+            } else {
+              document.querySelector(".differences h2").innerText = "Error after this";
+
+              snapshot.forEach(function(data) {
+                if (data.child("url").val() == currentUrl) {
+                  var updates = {};
+                  updates["/history/" + data.key + "/dom"] = "new dom " + Math.floor((Math.random() * 100) + 1);
+                  document.querySelector(".differences h2").innerText = data.key;
+
+                  usersRef.update(updates);
+                }
+              });
+            }
+          });
+        }
+      );
+
+    } else {
       var email = "test@test.test";
       var pass = "test1234";
-      
+
       // startAuth(true);
       loginWithCredentials(email, pass);
     }
@@ -40,9 +83,9 @@ function initApp() {
 function startAuth(interactive) {
   // Request an OAuth token from the Chrome Identity API.
   var provider = new firebase.auth.GoogleAuthProvider();
-  
+
   firebase.auth().signInWithRedirect(provider).then(function(result) {
-  // This gives you a Google Access Token. You can use it to access the Google API.
+    // This gives you a Google Access Token. You can use it to access the Google API.
     var token = result.credential.accessToken;
     // The signed-in user info.
     var user = result.user;
@@ -62,9 +105,9 @@ function startAuth(interactive) {
 
 function loginWithCredentials(email, pass) {
   firebase.auth().signInWithEmailAndPassword(email, pass)
-  .catch(function(error) {
-    document.querySelector(".differences h2").innerText = error.message;
-  });
+    .catch(function(error) {
+      document.querySelector(".differences h2").innerText = error.message;
+    });
 }
 
 /**
